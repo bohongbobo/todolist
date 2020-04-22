e162c09c5cd099b01671b315fe4c0a9c6ec56f2c
Bohong Li <bl447512@ohio.edu>
2020-04-22 18:52:16 -0500

    Updates

---
+++ precache-manifest.cba139506a82b888312c9d891d804a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0dd93390ebbf41b26fba1a8abc7104e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "61eef7cc6b3570e1f6c4",
+    "url": "/todolist/static/css/main.c704c058.chunk.css"
+  },
+  {
+    "revision": "5050a9689e23e639e5e3",
+    "url": "/todolist/static/js/2.f1e9364b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.f1e9364b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eef7cc6b3570e1f6c4",
+    "url": "/todolist/static/js/main.f9b0fef4.chunk.js"
+  },
+  {
+    "revision": "b97d7cb31fa7e5a5931e",
+    "url": "/todolist/static/js/runtime-main.811a1a38.js"
+  }
+]);
